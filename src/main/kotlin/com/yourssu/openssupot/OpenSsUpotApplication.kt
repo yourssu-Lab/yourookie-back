96677b4502c782330e133ef407009dec516fc0c1
권예진 <96688810+kwonyj1022@users.noreply.github.com>
2024-11-16 21:38:47 +0900

    chore: #1 프로젝트 초기 세팅 (#2)
    * chore: 스프링부트 프로젝트 생성
    
    * chore: .gitignore 파일 추가
    
    * chore: JPA 관련 로컬 프로파일 설정 추가

---
+++ src/main/kotlin/com/yourssu/openssupot/OpenSsUpotApplication.kt
@@ -0,0 +1,11 @@
+package com.yourssu.openssupot
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OpenSsUpotApplication
+
+fun main(args: Array<String>) {
+	runApplication<OpenSsUpotApplication>(*args)
+}
