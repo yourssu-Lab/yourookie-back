c05fa995a3c9cf072c16407f7271e14e7f84f72d
권예진 <96688810+kwonyj1022@users.noreply.github.com>
2024-11-17 06:13:26 +0900

    feat: #13 비밀번호 암호화 기능 추가 (#14)
    * feat: 비밀번호 암호화 기능 추가
    
    * feat: 비밀번호 일치 여부 판별 기능 추가
    
    * style: 이중 개행 제거

---
+++ src/main/kotlin/com/yourssu/openssupot/domain/support/security/password/PasswordEncoder.kt
@@ -0,0 +1,7 @@
+package com.yourssu.openssupot.domain.support.security.password
+
+interface PasswordEncoder {
+
+    fun encode(rawPassword: String): String
+    fun matches(rawPassword: String, encodedPassword: String): Boolean
+}
