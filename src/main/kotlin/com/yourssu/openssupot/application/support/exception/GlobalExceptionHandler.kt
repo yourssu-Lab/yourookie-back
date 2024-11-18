a2aa6bbe0ca483e90e5d809a05a549808556d7df
kwonyj1022 <kwonyj1022@naver.com>
2024-11-18 11:21:04 +0900

    feat: 인증된 단체 정보를 반환하는 ArgumentResolver 추가

--- src/main/kotlin/com/yourssu/openssupot/application/support/exception/GlobalExceptionHandler.kt
+++ src/main/kotlin/com/yourssu/openssupot/application/support/exception/GlobalExceptionHandler.kt
@@ -1,5 +1,6 @@
 package com.yourssu.openssupot.application.support.exception
 
+import com.yourssu.openssupot.application.support.authentication.LoginRequiredException
 import com.yourssu.openssupot.application.support.authentication.NoSuchOrganizationException
 import com.yourssu.openssupot.domain.domain.authentication.PasswordNotMatchException
 import com.yourssu.openssupot.domain.domain.file.InvalidFileException
@@ -24,6 +25,12 @@ import org.springframework.web.bind.annotation.RestControllerAdvice
 
 @RestControllerAdvice
 class GlobalExceptionHandler {
+
+    @ExceptionHandler(LoginRequiredException::class)
+    fun handleLoginRequiredException(e: LoginRequiredException): ResponseEntity<ExceptionResponse> {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+            .body(ExceptionResponse(e.message))
+    }
 
     @ExceptionHandler(NoSuchOrganizationException::class)
     fun handleNoSuchOrganizationException(e: NoSuchOrganizationException): ResponseEntity<ExceptionResponse> {
