cfa3d838add308c44c5f62609f319944ef1992a6
kwonyj1022 <kwonyj1022@naver.com>
2024-11-18 11:13:29 +0900

    feat: 인증을 위한 Interceptor 추가

--- src/main/kotlin/com/yourssu/openssupot/application/support/exception/GlobalExceptionHandler.kt
+++ src/main/kotlin/com/yourssu/openssupot/application/support/exception/GlobalExceptionHandler.kt
@@ -1,5 +1,6 @@
 package com.yourssu.openssupot.application.support.exception
 
+import com.yourssu.openssupot.application.support.authentication.NoSuchOrganizationException
 import com.yourssu.openssupot.domain.domain.authentication.PasswordNotMatchException
 import com.yourssu.openssupot.domain.domain.file.InvalidFileException
 import com.yourssu.openssupot.domain.domain.file.ReadFailureException
@@ -23,6 +24,12 @@ import org.springframework.web.bind.annotation.RestControllerAdvice
 
 @RestControllerAdvice
 class GlobalExceptionHandler {
+
+    @ExceptionHandler(NoSuchOrganizationException::class)
+    fun handleNoSuchOrganizationException(e: NoSuchOrganizationException): ResponseEntity<ExceptionResponse> {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+            .body(ExceptionResponse(e.message))
+    }
 
     @ExceptionHandler(PasswordNotMatchException::class)
     fun handlePasswordNotMatchException(e: PasswordNotMatchException): ResponseEntity<ExceptionResponse> {
--- src/main/kotlin/com/yourssu/openssupot/domain/domain/authentication/PrivateClaims.kt
+++ src/main/kotlin/com/yourssu/openssupot/domain/domain/authentication/PrivateClaims.kt
@@ -1,11 +1,17 @@
 package com.yourssu.openssupot.domain.domain.authentication
 
+import io.jsonwebtoken.Claims
+
 data class PrivateClaims(
     val userId: Long,
 ) {
 
     companion object {
         private const val USER_ID_KEY_NAME = "userId"
+
+        fun from(claims: Claims): PrivateClaims {
+            return PrivateClaims((claims[USER_ID_KEY_NAME] as Number).toLong())
+        }
     }
 
     fun toMap(): Map<String, Any> {
