005fa9852c0ea2f04fc84217b0f67fafdbca22cf
kwonyj1022 <kwonyj1022@naver.com>
2024-11-19 11:42:23 +0900

    feat: 단체 id로 단체의 공간 목록을 반환하는 repository 메서드 추가

--- src/main/kotlin/com/yourssu/openssupot/domain/domain/space/SpaceRepository.kt
+++ src/main/kotlin/com/yourssu/openssupot/domain/domain/space/SpaceRepository.kt
@@ -3,4 +3,5 @@ package com.yourssu.openssupot.domain.domain.space
 interface SpaceRepository {
 
     fun save(space: Space): Space
+    fun findAllByOrganizationId(organizationId: Long): List<Space>
 }
