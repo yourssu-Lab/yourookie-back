30dd9f4dc440b6659096942148fa8539402c30e4
kwonyj1022 <kwonyj1022@naver.com>
2024-11-20 02:23:07 +0900

    feat: 공간과 시간 범위에 예약이 존재하는지 조회하는 repository 메서드 추가

--- src/main/kotlin/com/yourssu/openssupot/storage/domain/reservation/ReservationRepositoryImpl.kt
+++ src/main/kotlin/com/yourssu/openssupot/storage/domain/reservation/ReservationRepositoryImpl.kt
@@ -2,6 +2,7 @@ package com.yourssu.openssupot.storage.domain.reservation
 
 import com.yourssu.openssupot.domain.domain.reservation.Reservation
 import com.yourssu.openssupot.domain.domain.reservation.ReservationRepository
+import java.time.LocalDateTime
 import org.springframework.stereotype.Repository
 
 @Repository
@@ -11,5 +12,13 @@ class ReservationRepositoryImpl(
 
     override fun save(reservation: Reservation): Reservation {
         return jpaReservationRepository.save(ReservationEntity.from(reservation)).toDomain()
+    }
+
+    override fun existsBySpaceIdAndDateRange(
+        spaceId: Long,
+        startDateTime: LocalDateTime,
+        endDateTime: LocalDateTime
+    ): Boolean {
+        return jpaReservationRepository.existsBySpaceIdAndDateRange(spaceId, startDateTime, endDateTime)
     }
 }
---
+++ src/test/kotlin/com/yourssu/openssupot/storage/domain/reservation/ReservationRepositoryImplTest.kt
@@ -0,0 +1,152 @@
+package com.yourssu.openssupot.storage.domain.reservation
+
+import com.yourssu.openssupot.domain.domain.organization.Email
+import com.yourssu.openssupot.domain.domain.organization.Organization
+import com.yourssu.openssupot.domain.domain.organization.OrganizationName
+import com.yourssu.openssupot.domain.domain.organization.OrganizationRepository
+import com.yourssu.openssupot.domain.domain.reservation.Reservation
+import com.yourssu.openssupot.domain.domain.reservation.ReservationRepository
+import com.yourssu.openssupot.domain.domain.reservation.ReservationTime
+import com.yourssu.openssupot.domain.domain.space.Capacity
+import com.yourssu.openssupot.domain.domain.space.Space
+import com.yourssu.openssupot.domain.domain.space.SpaceOperatingTime
+import com.yourssu.openssupot.domain.domain.space.SpaceRepository
+import com.yourssu.openssupot.storage.domain.organization.JpaOrganizationRepository
+import com.yourssu.openssupot.storage.domain.organization.OrganizationRepositoryImpl
+import com.yourssu.openssupot.storage.domain.space.JpaSpaceRepository
+import com.yourssu.openssupot.storage.domain.space.SpaceRepositoryImpl
+import java.time.LocalDateTime
+import java.time.LocalTime
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+
+@DataJpaTest
+@Suppress("NonAsciiCharacters")
+class ReservationRepositoryImplTest {
+
+    @Autowired
+    private lateinit var jpaOrganizationRepository: JpaOrganizationRepository
+
+    @Autowired
+    private lateinit var jpaSpaceRepository: JpaSpaceRepository
+
+    @Autowired
+    private lateinit var jpaReservationRepository: JpaReservationRepository
+
+    private lateinit var organizationRepository: OrganizationRepository
+    private lateinit var spaceRepository: SpaceRepository
+    private lateinit var reservationRepository: ReservationRepository
+
+    @BeforeEach
+    fun setup() {
+        organizationRepository = OrganizationRepositoryImpl(jpaOrganizationRepository)
+        spaceRepository = SpaceRepositoryImpl(jpaSpaceRepository)
+        reservationRepository = ReservationRepositoryImpl(jpaReservationRepository)
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        "2024-11-20T10:00, 2024-11-20T12:00",
+        "2024-11-20T12:00, 2024-11-20T14:00",
+        "2024-11-20T14:00, 2024-11-20T16:00",
+        "2024-11-20T14:00, 2024-11-20T16:00",
+        "2024-11-20T16:00, 2024-11-20T18:00"
+
+    )
+    fun `주어진 공간에 주어진 시간 범위와 겹치는 예약이 존재하면 true를 반환한다`(
+        startDateTime: LocalDateTime,
+        endDateTime: LocalDateTime
+    ) {
+        // given
+        val organization = saveOrganization("email@naver.com")
+        val space = saveSpace(organization, LocalTime.of(10, 0), LocalTime.of(18, 0))
+        saveReservation(
+            space,
+            LocalDateTime.of(2024, 11, 20, 11, 0),
+            LocalDateTime.of(2024, 11, 20, 13, 0),
+        )
+        saveReservation(
+            space,
+            LocalDateTime.of(2024, 11, 20, 15, 0),
+            LocalDateTime.of(2024, 11, 20, 17, 0),
+        )
+
+        // when
+        val result = reservationRepository.existsBySpaceIdAndDateRange(space.id!!, startDateTime, endDateTime)
+
+        // then
+        assertThat(result).isTrue()
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        "2024-11-20T10:00, 2024-11-20T11:00",
+        "2024-11-20T13:00, 2024-11-20T14:00",
+        "2024-11-20T14:00, 2024-11-20T15:00",
+        "2024-11-20T17:00, 2024-11-20T18:00",
+
+    )
+    fun `주어진 공간에 주어진 시간 범위와 겹치는 예약이 존재하지 않으면 false를 반환한다`(
+        startDateTime: LocalDateTime,
+        endDateTime: LocalDateTime
+    ) {
+        // given
+        val organization = saveOrganization("email@naver.com")
+        val space = saveSpace(organization, LocalTime.of(10, 0), LocalTime.of(18, 0))
+        saveReservation(
+            space,
+            LocalDateTime.of(2024, 11, 20, 11, 0),
+            LocalDateTime.of(2024, 11, 20, 13, 0),
+        )
+        saveReservation(
+            space,
+            LocalDateTime.of(2024, 11, 20, 15, 0),
+            LocalDateTime.of(2024, 11, 20, 17, 0),
+        )
+
+        // when
+        val result = reservationRepository.existsBySpaceIdAndDateRange(space.id!!, startDateTime, endDateTime)
+
+        // then
+        assertThat(result).isFalse()
+    }
+
+    private fun saveSpace(organization: Organization, openTime: LocalTime, closeTime: LocalTime): Space =
+        spaceRepository.save(
+            Space(
+                id = null,
+                organization = organization,
+                name = "name",
+                location = "location",
+                spaceImageUrl = "https://example.com/space.png",
+                operatingTime = SpaceOperatingTime(openTime, closeTime),
+                capacity = Capacity(10),
+            )
+        )
+
+    fun saveOrganization(email: String): Organization = organizationRepository.save(
+        Organization(
+            id = null,
+            email = Email(email),
+            encryptedPassword = "\$2a\$10\$SG1qTzy5vDOLYaPQ5ws/aA+K1mG2ekX+IuE8EXk/xhF0RQoNlXsXl",
+            name = OrganizationName("조직"),
+            logoImageUrl = "https://example.com/logo.png",
+            description = "조직입니다.",
+            encryptedReservationPassword = "\$2a\$10\$SG1qTzy5vDOLYaPQ5ws/aA+K1mG2ekX+IuE8EXk/xhF0RQoNlXsXl",
+        )
+    )
+
+    private fun saveReservation(space: Space, startDateTime: LocalDateTime, endDateTime: LocalDateTime): Reservation =
+        reservationRepository.save(
+            Reservation(
+                id = null,
+                space = space,
+                bookerName = "booker",
+                reservationTime = ReservationTime(startDateTime, endDateTime),
+            )
+        )
+}
