0fa4a18e081a065fd2e6c74d6dd437da5d61bb53
kwonyj1022 <kwonyj1022@naver.com>
2024-11-20 05:12:38 +0900

    fix: 주어진 공간의 주어진 날짜에 존재하는 예약 목록 조회 api 추가

--- src/main/kotlin/com/yourssu/openssupot/application/domain/reservation/ReservationController.kt
+++ src/main/kotlin/com/yourssu/openssupot/application/domain/reservation/ReservationController.kt
@@ -1,12 +1,16 @@
 package com.yourssu.openssupot.application.domain.reservation
 
+import com.yourssu.openssupot.domain.domain.reservation.ReadReservationsResult
 import com.yourssu.openssupot.domain.domain.reservation.ReservationService
 import jakarta.validation.Valid
 import java.net.URI
+import java.time.LocalDate
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -22,5 +26,16 @@ class ReservationController(
         val reservationId = reservationService.create(request.toCommand(spaceId))
 
         return ResponseEntity.created(URI.create("/spaces/$spaceId/reservations/$reservationId")).build()
+    }
+
+    @GetMapping("/spaces/{spaceId}/reservations")
+    fun readAllByDate(
+        @PathVariable spaceId: Long,
+        @RequestParam date: LocalDate,
+    ): ResponseEntity<List<ReadReservationResponse>> {
+        val result: ReadReservationsResult = reservationService.readAllByDate(spaceId, date)
+        val response: List<ReadReservationResponse> = result.reservationDtos.map { ReadReservationResponse.from(it) }
+
+        return ResponseEntity.ok(response)
     }
 }
