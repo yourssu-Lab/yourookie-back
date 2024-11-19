f965b3724d7fe5df35fc948648848f853ab512e2
kwonyj1022 <kwonyj1022@naver.com>
2024-11-20 02:48:47 +0900

    feat: 공간 id로 공간을 조회하는 repository 메서드 추가

--- src/main/kotlin/com/yourssu/openssupot/domain/domain/space/SpaceRepository.kt
+++ src/main/kotlin/com/yourssu/openssupot/domain/domain/space/SpaceRepository.kt
@@ -3,5 +3,6 @@ package com.yourssu.openssupot.domain.domain.space
 interface SpaceRepository {
 
     fun save(space: Space): Space
+    fun findById(spaceId: Long): Space?
     fun findAllByOrganizationId(organizationId: Long): List<Space>
 }
