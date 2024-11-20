2ab9f32b5ca4e0fcd8960ca6b35ad350b19a2dda
kwonyj1022 <kwonyj1022@naver.com>
2024-11-20 19:11:31 +0900

    feat: 단체 정보 수정 api 추가

--- src/main/kotlin/com/yourssu/openssupot/application/domain/organization/OrganizationController.kt
+++ src/main/kotlin/com/yourssu/openssupot/application/domain/organization/OrganizationController.kt
@@ -1,5 +1,7 @@
 package com.yourssu.openssupot.application.domain.organization
 
+import com.yourssu.openssupot.application.support.authentication.AuthenticationOrganization
+import com.yourssu.openssupot.application.support.authentication.AuthenticationOrganizationInfo
 import com.yourssu.openssupot.domain.domain.organization.OrganizationService
 import com.yourssu.openssupot.domain.domain.organization.ReadOrganizationsResult
 import jakarta.validation.Valid
@@ -7,6 +9,8 @@ import java.net.URI
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PatchMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RequestPart
@@ -48,5 +52,20 @@ class OrganizationController(
         }
 
         return ResponseEntity.status(HttpStatus.OK).body(response)
+    }
+
+    @PatchMapping("/organizations/{organizationId}")
+    fun update(
+        @AuthenticationOrganization authInfo: AuthenticationOrganizationInfo,
+        @PathVariable organizationId: Long,
+        @RequestPart(required = false) image: MultipartFile?,
+        @RequestPart @Valid request: UpdateOrganizationRequest,
+    ): ResponseEntity<Unit> {
+        organizationService.update(
+            authInfo.organizationId,
+            request.toCommand(organizationId, image)
+        )
+
+        return ResponseEntity.ok().build()
     }
 }
