6a2b9a536915eb3255ae7865c2863bf35afd90cc
kwonyj1022 <kwonyj1022@naver.com>
2024-11-21 00:03:51 +0900

    style: 코드 정렬

--- src/main/kotlin/com/yourssu/openssupot/application/domain/space/ReadSpaceResponse.kt
+++ src/main/kotlin/com/yourssu/openssupot/application/domain/space/ReadSpaceResponse.kt
@@ -17,45 +17,45 @@ data class ReadSpaceResponse(
         }
     }
 
-data class OrganizationItemResponse(
+    data class OrganizationItemResponse(
 
-    val id: Long,
-    val name: String,
-    val logoImageUrl: String,
-    val description: String?,
-    val hashtags: List<String> = emptyList(),
-) {
-    companion object {
-        fun from(dto: OrganizationDto) = OrganizationItemResponse(
-            id = dto.id,
-            name = dto.name,
-            logoImageUrl = dto.logoImageUrl,
-            description = dto.description,
-            hashtags = dto.hashtags,
-        )
+        val id: Long,
+        val name: String,
+        val logoImageUrl: String,
+        val description: String?,
+        val hashtags: List<String> = emptyList(),
+    ) {
+        companion object {
+            fun from(dto: OrganizationDto) = OrganizationItemResponse(
+                id = dto.id,
+                name = dto.name,
+                logoImageUrl = dto.logoImageUrl,
+                description = dto.description,
+                hashtags = dto.hashtags,
+            )
+        }
     }
-}
 
-data class SpaceItemResponse(
+    data class SpaceItemResponse(
 
-    val id: Long,
-    val name: String,
-    val spaceImageUrl: String,
-    val location: String,
-    val openingTime: LocalTime,
-    val closingTime: LocalTime,
-    val capacity: Int,
-) {
-    companion object {
-        fun from(dto: SpaceDto) = SpaceItemResponse(
-            id = dto.id,
-            name = dto.name,
-            spaceImageUrl = dto.spaceImageUrl?: dto.organization.logoImageUrl,
-            location = dto.location,
-            openingTime = dto.openingTime,
-            closingTime = dto.closingTime,
-            capacity = dto.capacity,
-        )
+        val id: Long,
+        val name: String,
+        val spaceImageUrl: String,
+        val location: String,
+        val openingTime: LocalTime,
+        val closingTime: LocalTime,
+        val capacity: Int,
+    ) {
+        companion object {
+            fun from(dto: SpaceDto) = SpaceItemResponse(
+                id = dto.id,
+                name = dto.name,
+                spaceImageUrl = dto.spaceImageUrl ?: dto.organization.logoImageUrl,
+                location = dto.location,
+                openingTime = dto.openingTime,
+                closingTime = dto.closingTime,
+                capacity = dto.capacity,
+            )
+        }
     }
-}
 }
