db23afd00dff7fd79a57643bfd0eefee33fae6b7
kwonyj1022 <kwonyj1022@naver.com>
2024-11-22 01:30:43 +0900

    feat: 예약 삭제 api 추가

--- src/main/kotlin/com/yourssu/openssupot/application/domain/reservation/ReservationController.kt
+++ src/main/kotlin/com/yourssu/openssupot/application/domain/reservation/ReservationController.kt
@@ -6,6 +6,7 @@ import jakarta.validation.Valid
 import java.net.URI
 import java.time.LocalDate
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
@@ -37,5 +38,15 @@ class ReservationController(
         val response: List<ReadReservationResponse> = result.reservationDtos.map { ReadReservationResponse.from(it) }
 
         return ResponseEntity.ok(response)
+    }
+
+    @DeleteMapping("/reservations/{reservationId}")
+    fun delete(
+        @PathVariable reservationId: Long,
+        @RequestBody @Valid request: DeleteReservationRequest,
+    ): ResponseEntity<Unit> {
+        reservationService.delete(reservationId, request.personalPassword)
+
+        return ResponseEntity.noContent().build()
     }
 }
