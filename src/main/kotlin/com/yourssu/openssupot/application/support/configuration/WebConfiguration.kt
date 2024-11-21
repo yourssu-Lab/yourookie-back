4caa583eb0249953be2e6bab31757a3c439583bc
kwonyj1022 <kwonyj1022@naver.com>
2024-11-21 11:28:15 +0900

    feat: cors 설정 추가

--- src/main/kotlin/com/yourssu/openssupot/application/support/configuration/WebConfiguration.kt
+++ src/main/kotlin/com/yourssu/openssupot/application/support/configuration/WebConfiguration.kt
@@ -3,7 +3,9 @@ package com.yourssu.openssupot.application.support.configuration
 import com.yourssu.openssupot.application.support.authentication.AuthenticationInterceptor
 import com.yourssu.openssupot.application.support.authentication.AuthenticationOrganizationInfoArgumentResolver
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpHeaders
 import org.springframework.web.method.support.HandlerMethodArgumentResolver
+import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
@@ -20,5 +22,17 @@ class WebConfiguration(
 
     override fun addArgumentResolvers(resolvers: MutableList<HandlerMethodArgumentResolver>) {
         resolvers.add(authenticationOrganizationInfoArgumentResolver)
+    }
+
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins(
+                "http://localhost:5173",
+                "https://localhost:5173",
+            )
+            .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+            .allowedHeaders("*")
+            .exposedHeaders(HttpHeaders.LOCATION)
+            .allowCredentials(true)
     }
 }
